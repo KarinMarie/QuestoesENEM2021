2662fb097236d88e44952d5d1498eefa4394bf57
Karin Marie <93265472+KarinMarie@users.noreply.github.com>
2021-10-27 11:47:44 -0300

    Add files via upload

---
+++ Trabalho 2C/Classes/Graficos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Trabalho_2C.Classes
+{
+    public class Graficos
+    {
+        Chart chartUsuario;
+
+        public Graficos(Chart chartUsuario)
+        {
+            this.chartUsuario = chartUsuario;
+        }
+
+        public void GraficosUsuario(int acertos, int erros, string materia)
+        {
+            chartUsuario.Titles["Materia"].Text = "";
+            chartUsuario.Series["Usuario"].Points.Clear();
+
+            chartUsuario.Titles["Materia"].Text = materia;
+
+            if (acertos == 0)
+            {
+                chartUsuario.Series["Usuario"].Points.AddXY("Erros", erros);
+                chartUsuario.Series["Usuario"].Points[0].Color = Color.MediumPurple;
+            }
+            else if (erros == 0)
+            {
+                chartUsuario.Series["Usuario"].Points.AddXY("Acertos", acertos);
+                chartUsuario.Series["Usuario"].Points[0].Color = Color.MediumAquamarine;
+            }
+            else
+            {
+                chartUsuario.Series["Usuario"].Points.AddXY("Acertos", acertos);
+                chartUsuario.Series["Usuario"].Points.AddXY("Erros", erros);
+                chartUsuario.Series["Usuario"].Points[0].Color = Color.MediumAquamarine;
+                chartUsuario.Series["Usuario"].Points[1].Color = Color.MediumPurple;
+            }
+        }
+    }
+}
---
+++ Trabalho 2C/Classes/Pergunta.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_2C
+{
+    class Pergunta
+    {
+        string diretorioAtual = Directory.GetCurrentDirectory() + @"\Questões\";
+        //string nomeQuestao;
+
+        public string Enunciado;
+        public string AltA;
+        public string AltB;
+        public string AltC;
+        public string AltD;
+        public string AltE;
+        public string Resolucao;
+        public string Letra;
+        public string Acertos;
+        public string Erros;
+
+        public Pergunta()
+        {
+
+        }
+
+        public Pergunta(string enun, string a, string b, string c, string d, string e, string res, string letra, string acertos, string erros)
+        {
+            Enunciado = enun;
+            AltA = a;
+            AltB = b;
+            AltC = c;
+            AltD = d;
+            AltE = e;
+            Resolucao = res;
+            Letra = letra;
+            Acertos = acertos;
+            Erros = erros;
+        }
+
+        public int GetNumeroQuestao(string nomeQuestao)
+        {
+            if (!Directory.Exists(diretorioAtual + nomeQuestao + @"\"))
+            {
+                MessageBox.Show("Escolha uma disciplina.");
+                return 0;
+            }
+
+            string[] diretorios = Directory.GetFiles(diretorioAtual + nomeQuestao + @"\");
+
+            return diretorios.Length;
+        }
+    }
+}
